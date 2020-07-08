bda40ff6df65cf11597ab27613fc9bf3dfd04076
mvolostnov <maxmv18@gmail.com>
2020-07-08 18:52:15 +0300

    Fixed issues with static data and DSL.
    To be updated...

--- src/main/java/com/apitests/test/data/entities/ClientData.java
+++ src/main/java/com/apitests/test/data/entities/ClientData.java
@@ -1,5 +1,8 @@
 package com.apitests.test.data.entities;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class ClientData {
     public static final String fullName = "Full_name_RFTxH";
     public static final String username = "Username_mOJGd";
--- src/main/java/com/apitests/webservices/Endpoints.java
+++ src/main/java/com/apitests/webservices/Endpoints.java
@@ -2,7 +2,7 @@ package com.apitests.webservices;
 
 public final class Endpoints {
 
-    public static final String login = " /login";
-    public static final String helloRequest = "/hello";
+    public final String login = " /login";
+    public final String helloRequest = "/hello";
 
 }
